e7f2bcfd04bd5d51564b6eab4e873c1f9d4e1e45
Benjaman Dutton <bdutton@oucs1423.otago.ac.nz>
2016-09-28 20:24:07 +1300

    Counting Up improved

--- CountingUp/Counting.java
+++ CountingUp/Counting.java
@@ -5,28 +5,19 @@
   */
 
 public class Counting {
-  
   public static void main (String[] args) {
-    
     long n = 0;
     long k = 0;
-    
     if (args.length == 2){
       n = Integer.valueOf(args[0]);
       k = Integer.valueOf(args[1]);
     }else {
       System.out.println("Error incorrect number of args");
     }
-    
-    for (long nc = 0; nc <= n; nc++) {
-      long nCk = 1;
-      for (long kc = 0; kc <= nc; kc++) {
-        if (n == nc && k == kc) System.out.print(nCk);
-        nCk = (nCk / (kc+1)) * (nc-kc);
-      }
-      
+    long nCk = 1;
+    for (long kc = 0; kc <= n; kc++) {
+      if (k == kc) System.out.println(nCk);
+      nCk = (nCk / (kc+1)) * (n-kc);
     }
-    System.out.println("\n " + Long.MAX_VALUE);
-    
   }
 }
